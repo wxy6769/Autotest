f1910c5e68d76e528ae1fd6e74501c749baeea56
HsinYu Wu <feather0522@gmail.com>
2019-11-11 10:58:52 +0800

    Module logging simple test.

--- _testGround.py
+++ _testGround.py
@@ -1,26 +1,10 @@
-import os
-import threading as th
+import logging
 
-# host = '127.0.0.1'
+logging.basicConfig(format='%(asctime)s [line:%(lineno)d] - %(levelname)s: %(message)s',
+ level=logging.DEBUG)
 
-
-def ping(host):
-    with open('cmd\\' + host + '.cmd', 'w') as file_ob:
-        file_ob.write('ping ' + host + '\n')
-    os.system('cmd\\' + host + '.cmd')
-
-
-if __name__ == '__main__':
-
-    # Dim list of threads.
-    threads = []
-    host = ['127.0.0.1', '8.8.8.8', '172.20.1.169']
-
-    # Dim multi-threads according to list of threads.
-    for i in range(len(host)):
-        threads.append(th.Thread(target=ping, args=(host[i],)))
-        threads[i].start()
-
-    # Main will wait for subs finished.
-    for i in range(len(threads)):
-        threads[i].join()
+logging.debug('debug information')
+logging.info('info')
+logging.warning('warning information')
+logging.error('error information')
+logging.critical('critical information')
