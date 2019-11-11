072732624897cc44c0edb3706f7d3526e75a7631
HsinYu Wu <feather0522@gmail.com>
2019-11-11 18:18:07 +0800

    Modified with the module integration.

--- multiup_mainTest.py
+++ multiup_mainTest.py
@@ -1,8 +1,8 @@
 import threading as th
 import settings as sets
-import snapavinfo as info
-import upgradefw as up
+import snapavcmd as sn
 import time as t
+
 
 # Get configures
 a = sets.getSets()
@@ -13,15 +13,15 @@ keys = ['model', 'host', 'user', 'pswd', 'cmd']
 
 # Content of one thread.
 def upfwtest(model, host, user, pswd, cmd):
-    ver = info.getVersion(model, host, user, pswd)
-    print(host + ':', model, ver)
-    # up.upgrade(host, user, pswd, cmd)
+    ver = sn.getVersion(model, host, user, pswd)
+    print(model + ':', host, ver)
+    # sn.cmdUpgrade(model, host, user, pswd, cmd)
     # t.sleep(80)
     # try:
-    #     ver = info.getVersion(model, host, user, pswd)
+    #     ver = sn.getVersion(model, host, user, pswd)
     # except Exception:
     #     t.sleep(20)
-    #     ver = info.getVersion(model, host, user, pswd)
+    #     ver = sn.getVersion(model, host, user, pswd)
     # print(host + ':', model, ver)
 
 
@@ -31,13 +31,14 @@ threads = []
 # Dim multi-threads according to list of threads.
 for i in range(len(a)):
     get_val = []
-    print('這是第', i + 1, '個執行緒設定')
+    # print('這是第', i + 1, '個執行緒設定')
     for k in keys:
         get_val.append(a[i].get(k))
 
-    print(get_val)
-    threads.append(th.Thread(target=upfwtest,
-        args=(get_val[0], get_val[1], get_val[2], get_val[3], get_val[4],)))
+    # print(get_val)
+    threads.append(th.Thread(
+        target=upfwtest, args=(
+            get_val[0], get_val[1], get_val[2], get_val[3], get_val[4],)))
     threads[i].start()
 
 # Main will wait for subs finished.
