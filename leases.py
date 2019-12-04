7ad860ee656f95f5dcd93d3afadb1caab9424cbb
HsinYu Wu <feather0522@gmail.com>
2019-12-04 11:53:22 +0800

    新增串列中重複元素的處理敘述

--- leases.py
+++ leases.py
@@ -5,36 +5,32 @@ def getLease():
     # 刪掉文件前段註解
     del(lease[:2])
 
-    # 此變數用於存取每組lease紀錄
-    innerlines = []
-    # 此變數用於存放所有lease紀錄
     all_sets = []
 
-    # 外圈-走訪文件，分行split存至list
     for i in range(len(lease)):
         innerlines = lease[i].split(';\n  ')
 
-        # 走訪list
         for j in range(len(innerlines)):
             try:
-                # 尋找lease紀錄中包含'client-hostname'的元素
                 if 'client-hostname' in innerlines[j]:
 
                     # 先針對IP字串進行處理
                     place_ne = innerlines[0].find(' {')
                     innerlines[0] = innerlines[0][:place_ne]
-
-                    # 再針對Model Name進行處理
-                    for x in innerlines:
-                        place_ne = innerlines[j].find(';')
-                        innerlines[j] = innerlines[j][:place_ne]
-                        break
+                    # 再對Model Name進行處理
+                    place_ne = innerlines[j].find(';')
+                    innerlines[j] = innerlines[j][:place_ne]
+                    innerlines[j] = innerlines[j].replace('client-hostname ', '').replace('"', '')
 
                     settings = {'host': innerlines[0], 'model': innerlines[j]}
                     all_sets.append(settings)
-
             except IndexError:
                 continue
+
+    for i in all_sets:
+        counter = all_sets.count(i)
+        if counter > 1:
+            del(all_sets[all_sets.index(i)])
 
     return(all_sets)
 
